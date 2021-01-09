509cf0126ecfde725c10accd83fc695f86798f51
gennady <gennady.gortalov@outlook.com>
2021-01-09 01:07:57 -0500

    new footer

--- specs/footer/footer.functional.spec.js
+++ specs/footer/footer.functional.spec.js
@@ -78,5 +78,8 @@ describe(" FOOTER FUNCTIONALITY ", ()=> {
     it("3.47 Validate that clicking on Footer link 'Alluma' redirects User to Alluma website https://www.alluma.org/",()=> {
       expect(Footer.getRedirectedUrl(Footer.allumaLink)).toEqual(expected.footer.allumaUrl)
     });
+    it("3.47 Validate that clicking on Footer link 'Alluma' redirects User to Alluma website https://www.alluma.org/",()=> {
+      expect(Footer.getRedirectedUrl(Footer.allumaLink)).toEqual(expected.footer.allumaUrl)
+    });
   })
 })
